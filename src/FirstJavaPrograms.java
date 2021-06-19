87e8e51e1a58b562b9e88b57276cc03eea341473
KiranKabadiLive <kirankabadi7@live.com>
2021-06-19 21:44:49 +0530

    This is a test of initial commit for this project.

--- src/FirstJavaPrograms.java
+++ src/FirstJavaPrograms.java
@@ -1,5 +1,6 @@
 public class FirstJavaPrograms {
     public static void main(String[] args) {
         System.out.println("This is my first program");
+        System.out.println("This is for my first commits");
     }
 }
